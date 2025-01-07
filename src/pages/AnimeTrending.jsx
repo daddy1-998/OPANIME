8d7610006d6c9e2696fd3bfd0ae8214e2972a74e
daddy1-998 <mounsifelmouden1998@gmail.com>
2025-01-07 22:29:04 +0100

    Update AnimeTrending.jsx

--- src/pages/AnimeTrending.jsx
+++ src/pages/AnimeTrending.jsx
@@ -7,11 +7,11 @@ function AnimeTrending() {
   const animes = [
     {
       id: 1,
-      title: "Solo Leveling Arise From Shadow",
-      image: "https://comicvine.gamespot.com/a/uploads/scale_large/11160/111607459/9275134-sololeveling-c022%28v08%29-p000%5Bcover%5D%5Bdig%5D%5Byenpress%5D%5Blucaz%5D.jpg",
+      title: "eminence in shadow",
+      image: "https://m.media-amazon.com/images/M/MV5BMDFmMWJlMWYtMDFkNi00OWViLTkzOGEtNDI0YWNmM2M5MzI5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
       rating: "4",
-      watch: "https://installchecker.com/cl/i/me4rmw",
-      download: "https://installchecker.com/cl/i/me4rmw"
+      watch: "https://sites.google.com/view/aniversestudio/trending-animes",
+      download: "https://sites.google.com/view/aniversestudio/trending-animes"
     }
   ]
 
@@ -31,12 +31,12 @@ function AnimeTrending() {
             const { title, image, type, rating, watch, download } = anime;
             return (
 
-              <div key={index} className='font-Poppins uppercase text-white w-[350px] h-[500px] bg-black-500 flex flex-col items-center gap-[15px] rounded-[10px] overflow-hidden'>
+              <div key={index} className='font-Poppins uppercase text-white w-[350px] h-[500px] bg-red-500 flex flex-col items-center gap-[15px] rounded-[10px] overflow-hidden'>
                 <div className='w-full h-[350px] '>
                   <img className='w-full h-full object-cover center ' src={image} alt={title} />
                 </div>
                 <div className='flex flex-col items-center gap-[20px]'>
-                  <h1 className=' font-bold  tracking-[1px]'>{title}</h1>
+                  <h1 className=' font-bold  tracking-[5px]'>{title}</h1>
                   <p className='flex items-center gap-4'>
                   {Array.from({ length: rating }, (_, index) => (
                     <FaStar key={index} className="text-yellow-500"  />
@@ -47,8 +47,8 @@ function AnimeTrending() {
                 </div>
                 <div className='p-[2%] w-full flex items-center justify-evenly gap-[20px] flex-row'>
 
-                  <a href={download} target="_blank" className='bg-black text-white  font-bold flex items-center gap-3 '><IoMdDownload/> Download</a>
-                  <a href={watch} target="_blank" className='bg-black text-white  font-bold flex items-center gap-3 '><BiMoviePlay/> Watch</a>
+                  <a href={download} target="_blank" className='bg-primary text-white  font-bold flex items-center gap-3 '><IoMdDownload/> Download</a>
+                  <a href={watch} target="_blank" className='bg-primary text-white  font-bold flex items-center gap-3 '><BiMoviePlay/> Watch</a>
 
                 </div>
               </div>
